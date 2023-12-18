d5cfa2e763b22d138d4b09c1b0409c4c34287c4e
Nora <nora.ekramy11@gmail.com>
2023-12-18 15:11:50 +0200

    moldel uodate

--- models/base_model.py
+++ models/base_model.py
@@ -6,37 +6,37 @@ import models
 
 
 class BaseModel:
-    """ca base class"""
+    """Base class for AirBnB clone"""
     def __init__(self, *args, **kwargs):
-        """initiates the class"""
-        if len(kwargs) > 0:
+        """Initializes the class"""
+        if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
                     continue
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.fromisoformat(value)
+                if key in ["created_at", "updated_at"]:
+                    try:
+                        value = datetime.fromisoformat(value)
+                    except ValueError:
+                        raise ValueError(f"Invalid datetime format for {key}")
                 setattr(self, key, value)
-            return
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        models.storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def save(self):
-        """update the time of any incoming update"""
+        """Updates the time of any incoming update"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def __str__(self):
-        """prints the name and id"""
-        return ("[{}] ({}) {}".format(
-                self.__class__.__name__, self.id, self.__dict__))
+        """Prints the class name and id"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def to_dict(self):
-        """return a dictionary containing all the base class attributes"""
-        new_dic = self.__dict__.copy()
-        new_dic["created_at"] = self.created_at.isoformat()
-        new_dic["updated_at"] = self.updated_at.isoformat()
-        new_dic["__class__"] = self.__class__.__name__
-        return new_dic
+        """Returns a dictionary containing all the class attributes"""
+        new_dict = self.__dict__.copy()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """base model test module"""
-import datetime
 import unittest
 from models.base_model import BaseModel
+from datetime import datetime
 
+class TestBaseModel(unittest.TestCase):
 
-class testBaseModel_init(unittest.TestCase):
-    """class for testing of base model instantiation"""
-    def test_two_models_unique_ids(self):
-        bm1 = BaseModel()
-        bm2 = BaseModel()
-        self.assertNotEqual(bm1.id, bm2.id)
+    def test_id_creation(self):
+        """Test that id is created correctly"""
+        model = BaseModel()
+        self.assertIsInstance(model.id, str)
 
+    def test_created_updated_at(self):
+        """Test creation and update timestamps"""
+        model = BaseModel()
+        self.assertIsInstance(model.created_at, datetime)
+        self.assertIsInstance(model.updated_at, datetime)
+        self.assertEqual(model.created_at, model.updated_at)
 
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
+
